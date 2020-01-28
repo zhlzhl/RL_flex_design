a69ebd9ccca2ebcbfe939eea2906b4c3e9b6b920
Lei <zhangleisk@gmail.com>
2020-01-27 22:54:08 -0500

    added bash script for running 2nd batch for Flexibility experiments 10x10

--- run_experiments10x10_CH1024-128_SPE6000_ITR80_EP500_2ndbatch.sh
+++ run_experiments10x10_CH1024-128_SPE6000_ITR80_EP500_2ndbatch.sh
@@ -6,9 +6,9 @@ for (( i=0; i<${#tarcs[@]}; i++ )); do
 python -m spinup.run_simple \
     --algo ppo  \
     --env Flexibility10x10T"${tarcs[i]}"-v0 \
-    --exp_name Flexibility10x10T"${tarcs[i]}"_PPO  \
+    --exp_name F10x10T"${tarcs[i]}"_PPO_CH1024-128_SPE6000_ITR80_EP500  \
     --cpu 8 \
-    --epochs 240  \
+    --epochs 500  \
     --steps_per_epoch 6000  \
     --save_freq 10  \
     --custom_h 1024-128 \
