41584dd45575dec8521e11bf9103c82dfda1c67c
Lei <zhangleisk@gmail.com>
2020-01-28 16:22:36 -0500

    added more bash scripts to run 20x20 with n_sample = 1 and n_sample=10

--- run_experiments20x20SP1_CH2048-512_SPE24000_ITR80_EP500_2ndbatch.sh
+++ run_experiments20x20SP1_CH2048-512_SPE24000_ITR80_EP500_2ndbatch.sh
@@ -4,12 +4,14 @@ tarcs=(44 48 52 56 60)
 for (( i=0; i<${#tarcs[@]}; i++ )); do
 python -m spinup.run_simple \
     --algo ppo  \
-    --env Flexibility20x20T"${tarcs[i]}"-v0 \
-    --exp_name Flexibility20x20T"${tarcs[i]}"_PPO  \
+    --env Flexibility20x20T"${tarcs[i]}"_SP1-v0 \
+    --exp_name F20x20T"${tarcs[i]}"SP1_PPO_CH2048-512_SPE24000_ITR80_EP500  \
     --cpu 8 \
-    --epochs 240  \
-    --steps_per_epoch 6000  \
+    --epochs 500  \
+    --steps_per_epoch 24000  \
     --save_freq 10  \
     --custom_h 2048-512 \
     --act tf.nn.relu  \
+    --train_pi_iters 80  \
+    --train_v_iters 80 \
     --do_checkpoint_eval; done
