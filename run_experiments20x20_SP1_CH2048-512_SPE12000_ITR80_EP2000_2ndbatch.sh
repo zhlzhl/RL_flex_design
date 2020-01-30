6644afd71ab637d76401224c2e073d74315037c9
Lei <zhangleisk@gmail.com>
2020-01-29 22:06:31 -0500

    added more script to only use limited n_sample during training but 5000 n_sample during evaluation

--- run_experiments20x20_SP1_CH2048-512_SPE12000_ITR80_EP2000_2ndbatch.sh
+++ run_experiments20x20_SP1_CH2048-512_SPE12000_ITR80_EP2000_2ndbatch.sh
@@ -4,11 +4,11 @@ tarcs=(44 48 52 56 60)
 for (( i=0; i<${#tarcs[@]}; i++ )); do
 python -m spinup.run_simple \
     --algo ppo  \
-    --env Flexibility20x20T"${tarcs[i]}"_SP10-v0 \
-    --exp_name F20x20T"${tarcs[i]}"_SP10_PPO_CH2048-512_SPE24000_ITR80_EP500  \
+    --env Flexibility20x20T"${tarcs[i]}"_SP1-v0 \
+    --exp_name F20x20T"${tarcs[i]}"_SP1_PPO_CH2048-512_SPE12000_ITR80_EP2000  \
     --cpu 8 \
-    --epochs 500  \
-    --steps_per_epoch 24000  \
+    --epochs 2000  \
+    --steps_per_epoch 12000  \
     --save_freq 10  \
     --custom_h 2048-512 \
     --act tf.nn.relu  \
--- run_experiments20x20_SP1_CH2048-512_SPE96000_ITR80_EP2000_2ndbatch.sh
+++ run_experiments20x20_SP1_CH2048-512_SPE96000_ITR80_EP2000_2ndbatch.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-tarcs=(20 24 28 32 36 40)
+tarcs=(44 48 52 56 60)
 for (( i=0; i<${#tarcs[@]}; i++ )); do
 python -m spinup.run_simple \
     --algo ppo  \
     --env Flexibility20x20T"${tarcs[i]}"_SP1-v0 \
-    --exp_name F20x20T"${tarcs[i]}"SP1_PPO_CH2048-512_SPE24000_ITR80_EP500  \
+    --exp_name F20x20T"${tarcs[i]}"_SP1_PPO_CH2048-512_SPE96000_ITR80_EP2000  \
     --cpu 8 \
-    --epochs 500  \
-    --steps_per_epoch 24000  \
+    --epochs 2000  \
+    --steps_per_epoch 96000  \
     --save_freq 10  \
     --custom_h 2048-512 \
     --act tf.nn.relu  \
