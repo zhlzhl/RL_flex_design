83836e561567ff3cb0b5cccc8e504d928ca7a6da
Lei <zhangleisk@gmail.com>
2020-02-01 22:00:57 -0500

    created two bash scripts to be run for 20x20 with SP50 SPE24000 and EP700

--- run_experiments20x20_SP50_CH2048-512_SPE24000_ITR80_EP700_2ndbatch.sh
+++ run_experiments20x20_SP50_CH2048-512_SPE24000_ITR80_EP700_2ndbatch.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-tarcs=(20 24 28 32 36 40)
+tarcs=(44 48 52 56 60)
 for (( i=0; i<${#tarcs[@]}; i++ )); do
 python -m spinup.run_simple \
     --algo ppo  \
     --env Flexibility20x20T"${tarcs[i]}"_SP50-v0 \
-    --exp_name F20x20T"${tarcs[i]}"_SP50_PPO_CH2048-512_SPE24000_ITR80_EP500  \
+    --exp_name F20x20T"${tarcs[i]}"_SP50_PPO_CH2048-512_SPE24000_ITR80_EP700  \
     --cpu 8 \
-    --epochs 500  \
+    --epochs 700  \
     --steps_per_epoch 24000  \
     --save_freq 10  \
     --custom_h 2048-512 \
     --act tf.nn.relu  \
     --train_pi_iters 80  \
     --train_v_iters 80 \
+    --eval_temp 0.1 \
+    --n_sample 50 \
     --do_checkpoint_eval; done
