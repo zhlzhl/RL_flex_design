8f73283465a24c985bba9e935065c0814bb98882
Lei <zhangleisk@gmail.com>
2020-02-01 21:23:27 -0500

    added an option to specify test env --env_name in test_policy.py so that I can choose which env to test with

--- spinup/utils/test_policy.py
+++ spinup/utils/test_policy.py
@@ -5,19 +5,21 @@ import os.path as osp
 import tensorflow as tf
 from spinup import EpochLogger
 from spinup.utils.logx import restore_tf_graph
+import gym
+import flexibility
 
-def load_policy(fpath, itr='last', deterministic=False, eval_temp=1.0, use_temp=True):
 
+def load_policy(fpath, itr='last', deterministic=False, eval_temp=1.0, use_temp=True, env_name=None):
     # handle which epoch to load from
-    if itr=='last':
-        saves = [int(x[11:]) for x in os.listdir(fpath) if 'simple_save' in x and len(x)>11]
-        itr = '%d'%max(saves) if len(saves) > 0 else ''
+    if itr == 'last':
+        saves = [int(x[11:]) for x in os.listdir(fpath) if 'simple_save' in x and len(x) > 11]
+        itr = '%d' % max(saves) if len(saves) > 0 else ''
     else:
-        itr = '%d'%itr
+        itr = '%d' % itr
 
     # load the things!
     sess = tf.Session()
-    model = restore_tf_graph(sess, osp.join(fpath, 'simple_save'+itr))
+    model = restore_tf_graph(sess, osp.join(fpath, 'simple_save' + itr))
 
     # get the correct op for executing actions
     if deterministic and 'mu' in model.keys():
@@ -32,22 +34,24 @@ def load_policy(fpath, itr='last', deterministic=False, eval_temp=1.0, use_temp=
     if not use_temp:
         get_action = lambda x: sess.run(action_op, feed_dict={model['x']: x[None, :]})[0]
     else:
-        get_action = lambda x : sess.run(action_op, feed_dict={model['x']: x[None,:],
-                                                               model['temperature']: eval_temp})[0]
+        get_action = lambda x: sess.run(action_op, feed_dict={model['x']: x[None, :],
+                                                              model['temperature']: eval_temp})[0]
 
-    # try to load environment from save
-    # (sometimes this will fail because the environment could not be pickled)
-    try:
-        state = joblib.load(osp.join(fpath, 'vars'+itr+'.pkl'))
-        env = state['env']
-    except:
-        env = None
+    if env_name is None:
+        # try to load environment from save
+        # (sometimes this will fail because the environment could not be pickled)
+        try:
+            state = joblib.load(osp.join(fpath, 'vars' + itr + '.pkl'))
+            env = state['env']
+        except:
+            env = None
+    else:
+        env = (lambda: gym.make(env_name))()
 
     return env, get_action
 
 
 def run_policy(env, get_action, max_ep_len=None, num_episodes=100, render=True):
-
     assert env is not None, \
         "Environment not found!\n\n It looks like the environment wasn't saved, " + \
         "and we can't run the agent in it. :( \n\n Check out the readthedocs " + \
@@ -67,7 +71,7 @@ def run_policy(env, get_action, max_ep_len=None, num_episodes=100, render=True):
 
         if d or (ep_len == max_ep_len):
             logger.store(EpRet=ep_ret, EpLen=ep_len)
-            print('Episode %d \t EpRet %.3f \t EpLen %d'%(n, ep_ret, ep_len))
+            print('Episode %d \t EpRet %.3f \t EpLen %d' % (n, ep_ret, ep_len))
             o, r, d, ep_ret, ep_len = env.reset(), 0, False, 0, 0
             n += 1
 
@@ -78,6 +82,7 @@ def run_policy(env, get_action, max_ep_len=None, num_episodes=100, render=True):
 
 if __name__ == '__main__':
     import argparse
+
     parser = argparse.ArgumentParser()
     parser.add_argument('fpath', type=str)
     parser.add_argument('--len', '-l', type=int, default=0)
@@ -87,10 +92,13 @@ if __name__ == '__main__':
     parser.add_argument('--deterministic', '-d', action='store_true')
     parser.add_argument('--eval_temp', type=float, default=1.0)
     parser.add_argument('--use_temp', action='store_true')
+    parser.add_argument('--env_name', type=str, default=None,
+                        help='manually specify an env for testing policy. this is optional. ')
     args = parser.parse_args()
-    env, get_action = load_policy(args.fpath, 
-                                  args.itr if args.itr >=0 else 'last',
+    env, get_action = load_policy(args.fpath,
+                                  args.itr if args.itr >= 0 else 'last',
                                   args.deterministic,
                                   args.eval_temp,
-                                  use_temp=args.use_temp)
-    run_policy(env, get_action, args.len, args.episodes, not(args.norender))
+                                  use_temp=args.use_temp,
+                                  env_name=args.env_name)
+    run_policy(env, get_action, args.len, args.episodes, not (args.norender))
