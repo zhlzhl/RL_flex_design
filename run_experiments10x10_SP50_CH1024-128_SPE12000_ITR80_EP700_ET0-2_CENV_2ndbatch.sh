f04867554a55401d886f080f905cc0216c4c0290
Lei <zhangleisk@gmail.com>
2020-02-04 15:55:53 -0500

    re-organized bash scripts
    10x10 uses the new custom env. while changes haven't been done to 20x20. 20x20 should be broken for now, as I didn't delete --n_sample.

--- run_experiments10x10_SP50_CH1024-128_SPE12000_ITR80_EP700_ET0-2_CENV_2ndbatch.sh
+++ run_experiments10x10_SP50_CH1024-128_SPE12000_ITR80_EP700_ET0-2_CENV_2ndbatch.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 #tarcs=(16 18 20 22 24)
-tarcs=(26 28 30 32 34)
+tarcs=(25 26 27 28 29 30 31 32 33 34 35 36)
 for (( i=0; i<${#tarcs[@]}; i++ )); do
 python -m spinup.run_simple \
     --algo ppo  \
-    --env Flexibility10x10T"${tarcs[i]}"-v0 \
-    --exp_name F10x10T"${tarcs[i]}"_PPO_CH1024-128_SPE6000_ITR80_EP500  \
+    --env Flexibility10x10T"${tarcs[i]}"_SP50-v0 \
+    --exp_name F10x10T"${tarcs[i]}"_SP50_PPO_CH1024-128_SPE12000_ITR80_EP500_ET0-2_CENV  \
     --cpu 8 \
     --epochs 500  \
-    --steps_per_epoch 6000  \
+    --steps_per_epoch 12000  \
     --save_freq 10  \
     --custom_h 1024-128 \
     --act tf.nn.relu  \
+    --eval_temp 0.2 \
     --do_checkpoint_eval; done
