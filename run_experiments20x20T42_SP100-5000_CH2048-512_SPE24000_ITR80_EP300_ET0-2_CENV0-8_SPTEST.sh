548aacfc57be738d6f5be1c73afe7804428442ec
Lei Zhang <zhangleisk@gmail.com>
2020-02-11 15:16:15 -0500

    created two bash scripts to run experiments with different n_sample for flexibilityenv n_smaple= 1, 50, 100, 5000

---
+++ run_experiments20x20T42_SP100-5000_CH2048-512_SPE24000_ITR80_EP300_ET0-2_CENV0-8_SPTEST.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sample=(100 5000)
+for (( i=0; i<${#sample[@]}; i++ )); do
+python -m spinup.run_simple \
+    --algo ppo  \
+    --env Flexibility20x20T42_SP"${sample[i]}"-v0 \
+    --exp_name F20x20T42_SP"${sample[i]}"_PPO_EP300_ET0-2_CENV0-8_SPTEST  \
+    --cpu 2 \
+    --epochs 300  \
+    --steps_per_epoch 24000  \
+    --save_freq 10  \
+    --custom_h 2048-512 \
+    --act tf.nn.relu  \
+    --eval_temp 0.2 \
+    --do_checkpoint_eval; done
