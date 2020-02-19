f9af8211a48e8a1405ed3e7b87463ba4d14d39a5
Lei <zhangleisk@gmail.com>
2020-02-19 14:51:03 -0500

    added scripts to test env2 for 10x10.

---
+++ run_experiments10x10_SP50_CH1024-128_SPE12000_ITR80_ET0-2_SMR0-8_ENV2_2ndbatch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#tarcs=(16 18 20 22 24)
+tarcs=(27 28 29 30 31 32)
+for (( i=0; i<${#tarcs[@]}; i++ )); do
+python -m spinup.run_simple \
+    --algo ppo  \
+    --env_name Flexibility10x10T"${tarcs[i]}"_SP50-v0 \
+    --exp_name F10x10T"${tarcs[i]}"_SP50_PPO_CH1024-128_SPE12000_ITR80_EP700_ET0-2_ENV2  \
+    --cpu 8 \
+    --epochs 700  \
+    --steps_per_epoch 12000  \
+    --save_freq 10  \
+    --custom_h 1024-128 \
+    --act tf.nn.relu  \
+    --eval_temp 0.2 \
+    --env_version 2 \
+    --do_checkpoint_eval; done
