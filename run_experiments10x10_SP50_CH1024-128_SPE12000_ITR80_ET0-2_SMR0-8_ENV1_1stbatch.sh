8fef45995527a18f7d7b9ca9da580b5f2d1270ee
Lei <zhangleisk@gmail.com>
2020-02-24 21:28:51 -0500

    scripts with multiple seeds.

--- run_experiments10x10_SP50_CH1024-128_SPE12000_ITR80_ET0-2_SMR0-8_ENV1_1stbatch.sh
+++ run_experiments10x10_SP50_CH1024-128_SPE12000_ITR80_ET0-2_SMR0-8_ENV1_1stbatch.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-tarcs=(16 17 18 19 20 21 22 23 24 25 26)
+tarcs=(10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 )
 # tarcs=(27 28 29 30 31 32 33 34 35 36)
 for (( i=0; i<${#tarcs[@]}; i++ )); do
 python -m spinup.run_simple \
     --algo ppo  \
-    --env Flexibility10x10T"${tarcs[i]}"_SP50-v0 \
-    --exp_name F10x10T"${tarcs[i]}"_SP50_PPO_CH1024-128_SPE12000_ITR80_EP500_ET0-2_CENV0-8  \
+    --env_name Flexibility10x10T"${tarcs[i]}"_SP50-v0 \
+    --exp_name F10x10T"${tarcs[i]}"_SP50_PPO_CH1024-128_SPE12000_EP800_ET0-2_SMR0-8_ENV1_2ndrun  \
     --cpu 8 \
-    --epochs 500  \
+    --epochs 800  \
     --steps_per_epoch 12000  \
     --save_freq 10  \
     --custom_h 1024-128 \
     --act tf.nn.relu  \
     --eval_temp 0.2 \
+    --num_runs 4 \
     --do_checkpoint_eval; done
