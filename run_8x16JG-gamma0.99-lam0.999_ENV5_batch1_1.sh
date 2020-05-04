3aa8fa9eb37291612e5da1af5420ebef11199754
Lei <zhangleisk@gmail.com>
2020-05-04 10:00:51 -0400

    scripts for 8x16JG

---
+++ run_8x16JG-gamma0.99-lam0.999_ENV5_batch1_1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+python -m spinup.run_flexibility   \
+                                   --algo ppo    \
+                                   --env_name F8x16JG-gamma0.99-lam0.999-v5   \
+                                   --exp_name F8x16JG-gamma0.99-lam0.999_CH1024-128_ENV5    \
+                                   --cpu 2   \
+                                   --epochs 800    \
+                                   --custom_h 1024-128   \
+                                   --env_version 5   \
+                                   --env_input input_JGm8n16_cv0.8.pkl   \
+                                   --target_arcs  22 25    \
+                                   --seed 100 110 120 130   \
+                                   --save_freq 10    \
+                                   --steps_per_epoch 18400   \
+                                   --do_checkpoint_eval  --gamma 0.99    --lam 0.999;
---
+++ run_8x16JG-gamma0.99-lam0.999_ENV5_batch1_entrypoint.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+for((i=0;i < 4;i++)); do bash run_8x16JG-gamma0.99-lam0.999_ENV5_batch1_${i}.sh & done
