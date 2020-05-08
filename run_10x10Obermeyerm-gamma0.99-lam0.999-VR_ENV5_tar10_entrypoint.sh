b0df808ddf27bd4ae4d82ddad8e7b8083af71278
Lei <zhangleisk@gmail.com>
2020-05-07 23:36:15 -0400

    removed some scripts

--- run_10x10Obermeyerm-gamma0.99-lam0.999-VR_ENV5_tar10_entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-for((i=0;i < 6;i++)); do bash run_10x10Obermeyerm-gamma0.99-lam0.999-VR_ENV5_tar10_${i}.sh & done
--- run_10x10Obermeyerm-gamma0.99-lam0.999_ENV5_tar10_entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-for((i=0;i < 8;i++)); do bash run_10x10Obermeyerm-gamma0.99-lam0.999_ENV5_tar10_${i}.sh & done
