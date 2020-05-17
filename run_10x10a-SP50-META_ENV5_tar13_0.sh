ef692ce052d5a00abab5fc3a75548c0cba8ef012
Lei <zhangleisk@gmail.com>
2020-05-16 21:29:48 -0400

    removed some scripts

--- run_10x10a-SP50-META_ENV5_tar13_0.sh
+++ run_10x10a-SP50-META_ENV5_tar13_0.sh
@@ -3,17 +3,17 @@
 python -m spinup.run_flexibility  \
                              --algo ppo  \
                              --env_name F10x10a-VR-SP50-v5  \
-                             --exp_name F10x10a-VR-SP50_CH1024-128_ENV5_tar22  \
+                             --exp_name F10x10a-VR-SP50-META_ENV5_tar13  \
                              --cpu 8 \
                              --epochs 800  \
                              --custom_h 1024-128  \
                              --env_version 5  \
+                             --target_arcs 13  \
                              --env_input input_ran10x10a_cv0.8.pkl  \
-                             --target_arcs  22  \
-                             --seed 10  \
+                             --seed 0  \
                              --save_freq 10   \
-                             --steps_per_epoch 17600  \
+                             --steps_per_epoch 10400  \
                              --do_checkpoint_eval \
-                             --env_subtract_full_flex  \
+                             --meta_learning  \
                              --gamma 0.99  \
                              --lam 0.999;
--- run_10x10a-VR-SP50-META_ENV5_tar13_0.sh
+++ run_10x10a-VR-SP50-META_ENV5_tar13_0.sh
@@ -2,18 +2,19 @@
 
 python -m spinup.run_flexibility  \
                              --algo ppo  \
-                             --env_name F10x10a-VR-SP50-v5  \
-                             --exp_name F10x10a-VR-SP50_CH1024-128_ENV5_tar13  \
+                             --env_name F10x10a-VR-SP50-META-v5  \
+                             --exp_name F10x10a-VR-SP50-META_ENV5_tar13  \
                              --cpu 8 \
                              --epochs 800  \
                              --custom_h 1024-128  \
-                             --env_version 5  \
+                             --env_version 6  \
                              --env_input input_ran10x10a_cv0.8.pkl  \
                              --target_arcs  13  \
-                             --seed 70  \
+                             --seed 0  \
                              --save_freq 10   \
                              --steps_per_epoch 10400  \
                              --do_checkpoint_eval \
                              --env_subtract_full_flex  \
+                             --meta_learning  \
                              --gamma 0.99  \
                              --lam 0.999;
